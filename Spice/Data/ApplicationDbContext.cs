a07dcb6f530cb971c3dbb4dedb6b02912d97e86c
Inksprout <candy.g24@gmail.com>
2019-05-31 15:29:23 +1000

    Spice tutorial

---
+++ Spice/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Spice.Models;
+
+namespace Spice.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Category> Category { get; set; }
+    }
+}
